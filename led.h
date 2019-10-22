b9b3fd72461df104883cc87177fd037d268b30ed
HARSH1911RATHORE <55412102+HARSH1911RATHORE@users.noreply.github.com>
2019-10-22 15:16:51 -0600

    Update led.h

--- led.h
+++ led.h
@@ -1,17 +1,5 @@
-//#ifndef _LED_H_
-//#define _LED_H_
-//#include <stdlib.h>
-//#include <stdio.h>
-//#include <stdint.h>
-//#include "board.h"
-//#include "peripherals.h"
-//#include "pin_mux.h"
-//#include "clock_config.h"
-//#include "MKL25Z4.h"
-//#include "fsl_debug_console.h"
-//void led_test();
-//void Delay(uint32_t time);
-//#endif
+
+
 #ifndef _LED_H_
 #define _LED_H_
 #include <stdlib.h>
